de160399b9b5be2b03086432024f86eb536c80ae
root <prueba@gmail.com>
2025-11-22 20:49:47 +0100

    microservicios usuarios

--- src/modulos/usuarios/usuarios.module.ts
+++ src/modulos/usuarios/usuarios.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ClientsModule, Transport } from '@nestjs/microservices'; // <--- Necesario
 import { UsuariosService } from './usuarios.service';
 import { UsuariosController } from './usuarios.controller';
 import { Usuario } from './entities/usuario.entity';
@@ -7,6 +8,16 @@ import { Usuario } from './entities/usuario.entity';
 @Module({
   imports: [
     TypeOrmModule.forFeature([Usuario]),
+    ClientsModule.register([
+      {
+        name: 'CLIENTES_SERVICE',
+        transport: Transport.TCP,
+        options: {
+          host: 'ms_clientes',
+          port: 3000,
+        },
+      },
+    ]),
   ],
   controllers: [UsuariosController],
   providers: [UsuariosService],
