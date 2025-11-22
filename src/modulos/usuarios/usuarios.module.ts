9bbb5a93d89f82cda2275a80a1f11ddf3193824f
root <prueba@gmail.com>
2025-11-22 20:45:04 +0100

    microservicios usuarios

--- src/modulos/usuarios/usuarios.module.ts
+++ src/modulos/usuarios/usuarios.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 import { UsuariosService } from './usuarios.service';
 import { UsuariosController } from './usuarios.controller';
 import { Usuario } from './entities/usuario.entity';
@@ -8,16 +7,6 @@ import { Usuario } from './entities/usuario.entity';
 @Module({
   imports: [
     TypeOrmModule.forFeature([Usuario]),
-    ClientsModule.register([
-      {
-        name: 'CLIENTES_SERVICE', 
-        transport: Transport.TCP,
-        options: {
-          host: 'localhost', 
-          port: 3000,
-        },
-      },
-    ]),
   ],
   controllers: [UsuariosController],
   providers: [UsuariosService],
--- src/modulos/usuarios/usuarios.service.ts
+++ src/modulos/usuarios/usuarios.service.ts
@@ -1,5 +1,4 @@
-import { Injectable, Inject } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
+import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Usuario } from './entities/usuario.entity';
@@ -9,8 +8,6 @@ export class UsuariosService {
   constructor(
     @InjectRepository(Usuario)
     private usuariosRepository: Repository<Usuario>,
-    // Inyectamos el cliente TCP (app.module.ts)
-    @Inject('CLIENTES_SERVICE') private readonly clientesClient: ClientProxy,
   ) {}
 
   async findAll() {
@@ -19,19 +16,6 @@ export class UsuariosService {
 
   async getPerfilCompleto(idInterno: string) {
     const usuario = await this.usuariosRepository.findOne({ where: { id: idInterno } });
-
-    if (!usuario || !usuario.clienteId) {
-        // Manejar error si no existe en BD_USUARIOS
-        return null;
-    }
-
-    // Pide los datos al otro Microservicio
-    const datosCliente = await this.clientesClient.send(
-      { cmd: 'obtener_datos_cliente' }, // El mismo patrón que definimos en mod-clientes
-      usuario.clienteId, // El ID LÓGICO que enviamos como payload
-    ).toPromise(); 
-
-    // Retorna un objeto consolidado
-    return { ...usuario, datosCliente };
+    return usuario;
   }
 }
