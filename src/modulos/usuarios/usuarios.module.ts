cece42686dae8a96b4279d2ba0f9d2ed18c7fe4d
root <prueba@gmail.com>
2025-11-22 20:36:44 +0100

    microservicios usuarios

--- src/modulos/usuarios/usuarios.module.ts
+++ src/modulos/usuarios/usuarios.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
-import { UsuariosController } from './usuarios.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsuariosService } from './usuarios.service';
+import { UsuariosController } from './usuarios.controller';
+import { Usuario } from './entities/usuario.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Usuario]), 
+  ],
   controllers: [UsuariosController],
   providers: [UsuariosService],
+  exports: [UsuariosService],
 })
 export class UsuariosModule {}
