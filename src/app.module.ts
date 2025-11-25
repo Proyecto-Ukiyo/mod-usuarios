f1cbb1e75606d7de87616d6d0302d036c7c6707a
root <prueba@gmail.com>
2025-11-25 11:00:34 +0100

    seed usuarios

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsuariosModule } from './modulos/usuarios/usuarios.module';
 import { Usuario } from './modulos/usuarios/entities/usuario.entity';
+import { ConfigModule } from '@nestjs/config';
 import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST, 
